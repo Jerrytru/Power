7cff06ee7cf7183aedea246901b9a15464df0471
Jerry Trujillo <trujillojerry70@yahoo.com>
2022-01-18 13:42:52 -0500

    Initial-setup

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+const App: () => Node = () => {
+
+  return (
+    <div><h1>Hello </h1></div>
+  );
+};
+
+const styles = StyleSheet.create({
+  // sectionContainer: {
+  //   marginTop: 32,
+  //   paddingHorizontal: 24,
+  // },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+});
+
+export default App;
